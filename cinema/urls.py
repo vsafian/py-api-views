d8ab990e21c1b0d88890f6b3d55e392e5cff11f9
Vitalii Safian <viisaf.prog.gt@gmail.com>
2025-03-03 11:41:08 +0200

    fix flake8 style error

--- cinema/urls.py
+++ cinema/urls.py
@@ -34,7 +34,10 @@ cinema_hall_detail = CinemaHallViewSet.as_view(
 urlpatterns = [
     path("", include(router.urls)),
     path("cinema_halls/", cinema_hall_list, name="cinema-hall-list"),
-    path("cinema_halls/<int:pk>/", cinema_hall_detail, name="cinema-hall-detail"),
+    path(
+        "cinema_halls/<int:pk>/",
+         cinema_hall_detail, name="cinema-hall-detail"
+    ),
     path("genres/", GenreList.as_view(), name="genres-list"),
     path("genres/<int:pk>/", GenreDetail.as_view(), name="genre-detail"),
     path("actors/", ActorList.as_view(), name="actors-list"),
